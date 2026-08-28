9fb2ac1addca04c489b237bda6240c7eff517b56
Totex <tonyfilpo@azet.sk>
2026-08-28 09:45:03 +0200

    Added comment to main.js

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,5 +1,5 @@
 // frontend/js/main.js
 import '../css/main.css';
 
-// Build JS and CSS files in production don't use the: /frontend folder
+// Build JS and CSS files, in production don't use the: /frontend folder
 console.log('Hello from main.js!');

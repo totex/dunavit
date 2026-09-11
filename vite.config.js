7a3b9c9697078d242da6b26cbdb1374410161702
Totex <tonyfilpo@azet.sk>
2026-09-11 11:34:42 +0200

    Changed vite config and small change in package.json

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,27 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [tailwindcss()],
-  build: {
-    outDir: 'dunavit/static',  // 👈 Adjust as needed
-    rollupOptions: {
-      input: {
-        main: 'frontend/js/main.js', // 👈 Adjust as needed
-      },
-      output: {
-        entryFileNames: 'js/dunavit.min.js', 
-        assetFileNames: 'css/dunavit.min.css',
-      },
+    plugins: [tailwindcss()],
+
+    build: {
+        outDir: 'dunavit/static',
+
+        watch: {
+            exclude: [
+                'dunavit/static/**',
+            ],
+        },
+
+        rollupOptions: {
+            input: {
+                main: 'frontend/js/main.js',
+            },
+
+            output: {
+                entryFileNames: 'js/dunavit.min.js',
+                assetFileNames: 'css/dunavit.min.css',
+            },
+        },
     },
-  },
 })
